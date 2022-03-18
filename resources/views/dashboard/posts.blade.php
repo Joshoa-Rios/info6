05ab355bb887d37e952139c4914cbe6a638d7bda
Joshoa-Rios <joshoagerardoriosvaldez@gmail.com>
2022-03-17 21:26:03 -0600

    Cambios Clase 3

--- resources/views/dashboard/posts.blade.php
+++ resources/views/dashboard/posts.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-<form action="{{route("post.store")}}" method="post">
-    <div class="form-group">
+@include('fragments.validation-errors')
+<h1>Post</h1>
+<form action="{{route("post.store") }}" method="post">
+  @csrf
+    <div class="mb-3">
       <label for="title" class="form-label">Título</label>
       <input type="text" class="form-control" name="title" id="title">
     </div>
       
-    <div class="form-group">
+    <div class="mb-3">
       <label for="url_clean">Url Limpia</label>
       <input type="text" class="form-control" name="url_clean" id="url_clean">
     </div>
 
-    <div class="form-group">
+    <div class="mb-3">
         <label for="content">Contenido</label>
         <textarea class="form-control" id="content" name="content" rows="3"></textarea>
     </div>
     
     <button type="submit" value="Enviar" class="btn btn-primary">Enviar</button>
   </form>
+
+
 @endsection
